c2ba0d7a16bf79cc5e9c23a4c41bc2f759e13457
unknown <chauhandevansh780@gmail.com>
2026-07-25 00:38:20 +0530

    learned serialization

--- backend/accounts/apiviews.py
+++ backend/accounts/apiviews.py
@@ -1,11 +1,18 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view          # Makes this function a DRF API
+from rest_framework.response import Response             # Returns JSON response
+
+from .models import employee                           # Import Employee model
+from .serializers import EmployeeSerializer             # Import serializer
 
 
-@api_view(["GET"])
-def hello(request):
-    return Response(
-        {
-            "message": "Hello from Django REST Framework"
-        }
+@api_view(["GET"])                                      # Allow only GET requests
+def employees(request):
+
+    employees = employee.objects.all()                  # Fetch all employees from database
+
+    serializer = EmployeeSerializer(                    # Convert Employee objects into JSON
+        employees,
+        many=True                                       
     )
+
+    return Response(serializer.data)                    
--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from .apiviews import hello
+from django.urls import path                          
+from .apiviews import employees                     
 
 urlpatterns = [
-    path('hello/', hello, name='hello')
+    path("employees/", employees),                     
 ]
