ee95d7ffc8c4a23469e254020ede8e1f553be9af
unknown <chauhandevansh780@gmail.com>
2026-07-28 11:43:39 +0530

    sign up sign in form created

--- backend/job/settings.py
+++ backend/job/settings.py
@@ -40,10 +40,13 @@ INSTALLED_APPS = [
 
     'rest_framework',
 
+    'corsheaders',
+
     'accounts',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',      
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,4 +128,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "job" / "static",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8081",                  # Allow requests from React (Vite)
 ]
