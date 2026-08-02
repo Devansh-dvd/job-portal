607ebe24330967497c99b72b6ba06ac8536d0c15
unknown <chauhandevansh780@gmail.com>
2026-08-02 11:22:01 +0530

    cloudinary integrated

--- backend/accounts/apiviews.py
+++ backend/accounts/apiviews.py
@@ -3,13 +3,27 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.decorators import parser_classes
-
 from .serializers import UserSerializer
+import cloudinary.uploader  
 
-@api_view(["POST"])
+@api_view(['POST'])
 @parser_classes([MultiPartParser, FormParser])
 def register(request):
-    serializer = UserSerializer(data=request.data)
+
+    data = request.data.copy()
+
+    profile_picture = request.FILES.get("profile_picture")
+    resume = request.FILES.get("resume")
+
+    if profile_picture:
+        result = cloudinary.uploader.upload(profile_picture)
+        data["profile_picture"] = result["secure_url"]
+
+    if resume:
+        result_resume = cloudinary.uploader.upload(resume)
+        data["resume"] = result_resume["secure_url"]
+
+    serializer = UserSerializer(data=data)
 
     if serializer.is_valid():
         serializer.save()
@@ -22,5 +36,4 @@ def register(request):
         )
 
     print(serializer.errors)
-
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
-    resume = models.FileField(upload_to='resumes/', null=True, blank=True)
+    profile_picture = models.URLField(null=True, blank=True)
+    resume = models.URLField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-
-    
-    
