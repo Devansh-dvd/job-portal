69659313b8b28b0f2f9f13298253af287a73e6fc
unknown <chauhandevansh780@gmail.com>
2026-08-21 22:05:16 +0530

    applications url

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .apiviews import register , login
+from .apiviews import register , login , create_application
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
 urlpatterns = [
     path('register/', register),
     path('login/', login),
+    path("applications/", create_application),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
